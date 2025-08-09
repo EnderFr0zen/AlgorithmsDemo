830dec3c0760933394c9e80cd4f5b360cfbf0a92
Hsing-Wei (Cynwell) Liao <cynwellliao.20011203@gmail.com>
2025-08-09 17:25:13 +1000

    part1(module2): add DynamicConnectivity

---
+++ src/part1/module2/DynamicConnectivity.java
@@ -0,0 +1,27 @@
+package part1.module2;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.UF;
+
+public class DynamicConnectivity {
+
+    public static void main(String[] args) {
+        // Read the number of elements (N) from standard input
+        int N = StdIn.readInt();
+        // Create a new Union-Find data structure with N elements (0 to N-1)
+        UF unionFind = new UF(N);
+        // Continue reading pairs of integers until input is exhausted
+        while (!StdIn.isEmpty()) {
+            // Read a pair of integers p and q
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            // If p and q are not already connected
+            if (!unionFind.connected(p, q)) {
+                // Connect p and q
+                unionFind.union(p, q);
+                // Print the connection that was just made
+                System.out.println(p + " " + q);
+            }
+        }
+    }
+}
