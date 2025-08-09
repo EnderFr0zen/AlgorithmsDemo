3eff8c5d712917ec4bed32777d32235e5ace7d8d
Hsing-Wei (Cynwell) Liao <cynwellliao.20011203@gmail.com>
2025-08-09 17:14:12 +1000

    part1(module1): add algs4.jar and add HelloWorld/HelloGoodbye/RandomWord

---
+++ src/part1/module1/RandomWord.java
@@ -0,0 +1,20 @@
+package part1.module1;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+
+    public static void main(String[] args) {
+        String champion = "";
+        int count = 0;
+
+        for (String word : args) {
+            count++;
+            if (StdRandom.bernoulli(1.0 / count)) {
+                champion = word;
+            }
+        }
+        System.out.println(champion);
+    }
+
+}
